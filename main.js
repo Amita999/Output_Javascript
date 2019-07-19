6ad572f950f8a63ce7bb9d24fb3f4cc1dfa50d3f
amita.jethani <amitajethani@gmail.com>
2019-07-19 09:06:12 +0530

    The program has bugs

---
+++ main.js
@@ -0,0 +1,8 @@
+//adding data in html element
+//We need to find Dom
+//document as object
+document.getElementById("Adding_data").innerHTML = "Adding Data by using the DOM";
+console.log();
+
+//WRITING CONTENT IN BROWSER
+document.write("Hello Amita");
